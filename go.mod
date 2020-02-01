47f93ba5a858772e7352d25954bc8be5f1cd69ae
Luc Michalski <lucmichalski@Lucs-MacBook-Air.local>
2020-02-01 14:43:52 +0100

    fetch github topics and repo info

--- go.mod
+++ go.mod
@@ -5,4 +5,14 @@ go 1.13
 require (
 	github.com/corpix/uarand v0.1.1 // indirect
 	github.com/gocolly/colly/v2 v2.0.1
+	github.com/google/go-github/v29 v29.0.2
+	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/sethgrid/pester v0.0.0-20190127155807-68a33a018ad0 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/sniperkit/cacher v0.0.0-20171213170759-61f2a1265daa
+	github.com/x0rzkov/go-vcsurl v1.0.1 // indirect
+	github.com/x0rzkov/httpcache v0.0.0-20200108145149-5a6ae9c4c311
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
--- main.go
+++ main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"context"
+	"os"
+	"time"
 
+	"github.com/k0kubun/pp"
 	"github.com/corpix/uarand"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/queue"
+	"github.com/google/go-github/v29/github"
+	log "github.com/sirupsen/logrus"
+	"github.com/x0rzkov/go-vcsurl"
+	"github.com/iancoleman/strcase"
+	ghclient "github.com/x0rzkov/gocolly-kitploit/pkg/client"
 )
 
+var (
+	clientManager   *ghclient.ClientManager
+	clientGH        *ghclient.GHClient
+	cachePath 		= "./data/httpcache"
+	debug 			= false
+	logLevelStr 	= "info"
+)
+
+type VcsInfo struct {
+	Desc string
+	URL string 
+	Lang string
+	Tags []string
+}
+
 func main() {
+
 	// Array containing all the known URLs in a sitemap
 	knownUrls := []string{}
 	githubUrls := []string{}
+
 
 	// Create a Collector specifically for Shopify
 	c := colly.NewCollector(
@@ -29,7 +56,7 @@ func main() {
 	)
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("visiting", r.URL)
+		log.Println("visiting", r.URL)
 	})
 
 	// Create a callback on the XPath query searching for the URLs
@@ -48,20 +75,107 @@ func main() {
 	// This class is unique to the div that holds all information about a story
 	c.OnHTML(".kiploit-download", func(e *colly.HTMLElement) {
 		// t := e.ChildAttr("a", "href")
-		fmt.Println("found ", e.Attr("href"))
-		githubUrls = append(githubUrls, e.Attr("href"))
+		log.Println("found href=", e.Attr("href"))
+		if strings.HasPrefix(e.Attr("href"), "https://github.com") {
+			githubUrls = append(githubUrls, e.Attr("href"))
+		}
 	})
 
 	q.AddURL("https://www.kitploit.com/sitemap.xml")
 
-	fmt.Println("All github URLs:")
+	log.Println("All github URLs:")
 
 	// Consume URLs
 	q.Run(c)
 
+	// github client init
+	clientManager = ghclient.NewManager(cachePath, []string{os.Getenv("GITHUB_TOKEN")})
+	defer clientManager.Shutdown()
+	clientGH = clientManager.Fetch()
+
+	var vcsRepos []*VcsInfo
 	for _, url := range githubUrls {
 		fmt.Println("\t", url)
+		if info, err := vcsurl.Parse(url); err == nil {
+			repoInfo, err := getInfo(clientGH.Client, info.Username, info.Name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			var topicCamel []string
+			topicCamel = append(topicCamel, *repoInfo.Language)
+			topics, err := getTopics(clientGH.Client, info.Username, info.Name)
+			if err == nil {
+				for _, topic := range topics {
+					topicCamel = append(topicCamel, strcase.ToCamel(strings.Replace(topic, "-", "", -1)))
+				}
+			}
+			vcsRepo := &VcsInfo{
+				Desc: *repoInfo.Description,
+				URL: url,
+				Tags: topicCamel,
+				Lang: *repoInfo.Language,
+			}
+			pp.Println(vcsRepo)			
+			vcsRepos = append(vcsRepos, vcsRepo)
+		}
 	}
-	fmt.Println("Collected", len(githubUrls), "URLs")
+	log.Println("Collected", len(githubUrls), "URLs")
 	
+}
+
+func getInfo(client *github.Client, owner, name string) (*github.Repository, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	waitForRemainingLimit(client, true, 10)
+	info, _, err := client.Repositories.Get(ctx, owner, name)
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
+}
+
+func getTopics(client *github.Client, owner, name string) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	waitForRemainingLimit(client, true, 10)
+	topics, _, err := client.Repositories.ListAllTopics(ctx, owner, name)
+	if err != nil {
+		return nil, err
+	}
+	return topics, nil
+}
+
+func waitForRemainingLimit(cl *github.Client, isCore bool, minLimit int) {
+	for {
+		rateLimits, _, err := cl.RateLimits(context.Background())
+		if err != nil {
+			if debug {
+				log.Printf("could not access rate limit information: %s\n", err)
+			}
+			<-time.After(time.Second * 1)
+			continue
+		}
+
+		var rate int
+		var limit int
+		if isCore {
+			rate = rateLimits.GetCore().Remaining
+			limit = rateLimits.GetCore().Limit
+		} else {
+			rate = rateLimits.GetSearch().Remaining
+			limit = rateLimits.GetSearch().Limit
+		}
+
+		if rate < minLimit {
+			if debug {
+				log.Printf("Not enough rate limit: %d/%d/%d\n", rate, minLimit, limit)
+			}
+			<-time.After(time.Second * 60)
+			continue
+		}
+		if debug {
+			log.Printf("Rate limit: %d/%d\n", rate, limit)
+		}
+		break
+	}
 }
